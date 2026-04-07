81b653b373dc7c53eed6ec32c2c142ac9a0a7ae8
vl.shagaliev <vl.shagaliev@yandex.ru>
2026-04-07 13:55:49 +0500

    Первый этап дипломного проекта

--- core/ewm-service/src/main/java/ru/practicum/ewm/EwmApplication.java
+++ core/ewm-service/src/main/java/ru/practicum/ewm/EwmApplication.java
@@ -2,11 +2,15 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {
         "ru.practicum.ewm",
         "ru.practicum.stats"
 })
+@ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class EwmApplication {
     public static void main(String[] args) {
         SpringApplication.run(EwmApplication.class, args);
